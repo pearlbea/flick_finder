9ab40f74a279427b3cfb2914771bf964358ad785
Pearl <pearlbea@gmail.com>
2018-11-24 19:36:29 -0600

    Update default test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flick_finder/main.dart';
+import 'package:flick_finder/home.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('HomePage test', (WidgetTester tester) async {
+    HomePage homePage = HomePage();
+    await tester.pumpWidget(MaterialApp(home: homePage));
+    expect(find.text('flick finder'), findsOneWidget);
   });
 }
