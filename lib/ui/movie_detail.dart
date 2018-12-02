f03a4762daffd38a7dc5e77aa5b3d388b7c292dd
Pearl <pearlbea@gmail.com>
2018-12-02 09:44:25 -0600

    Add movie detail page

---
+++ lib/ui/movie_detail.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../movie_service.dart';
+
+class MovieDetail extends StatelessWidget {
+  const MovieDetail(this.movie);
+  final dynamic movie;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  child: Image.network(
+                    MovieService().getBackdropPath(movie.backdropPath),
+                  ),
+                ),
+              ],
+            ),
+            Row(children: <Widget>[
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Text(movie.title, style: theme.textTheme.headline),
+                ),
+              ),
+            ]),
+            Row(children: <Widget>[
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                  child: Text(movie.overview,
+                      style: const TextStyle(height: 1.2, fontSize: 18.0)),
+                ),
+              )
+            ])
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// SizedBox(
+//   child: Hero(
+//     tag: movie.title,
+//     child:
+//         Image.network(MovieService().getPosterPath(movie.posterPath)),
+//   ),
+// )
