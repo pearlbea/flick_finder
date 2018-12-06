91a2658e3ad522f9effaab8dd5cffb147d0e026c
Pearl <pearlbea@gmail.com>
2018-12-06 17:25:19 -0600

    Add genre list

--- lib/movie_service.dart
+++ lib/movie_service.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
+import 'package:meta/meta.dart';
 import 'package:http/http.dart' as http;
 
 import 'app_config.dart';
+import 'model/genre_list.dart';
 import 'model/movies.dart';
 
 const String baseImagePath = 'https://image.tmdb.org/t/p/';
@@ -10,10 +12,23 @@ const String posterSize = 'w154';
 const String backdropSize = 'w780';
 
 class MovieService {
+  Future<String> apiKey;
+  Future<GenreList> genreList;
   Future<Movies> movieList;
 
-  String constructUrl(String apiKey) {
-    return '$baseUrl/discover/movie?api_key=$apiKey';
+  String constructUrl(
+      {@required String apiKey, @required String path, int genreId}) {
+    if (genreId != null) {
+      return '$baseUrl/$path?api_key=$apiKey&with_genres=$genreId';
+    }
+    return '$baseUrl/$path?api_key=$apiKey';
+  }
+
+  Future<String> loadAPIkey() async {
+    if (apiKey != null) {
+      return apiKey;
+    }
+    return getApiKey();
   }
 
   String getPosterPath(String posterId) {
@@ -24,13 +39,27 @@ class MovieService {
     return '$baseImagePath$backdropSize/$imageId';
   }
 
-  Future<Movies> getMovies() async {
-    if (movieList != null) {
-      return movieList;
+  Future<GenreList> getGenreList() async {
+    if (genreList != null) {
+      return genreList;
     }
 
-    movieList = getApiKey()
-        .then((String apiKey) => constructUrl(apiKey))
+    genreList = loadAPIkey()
+        .then((String apiKey) {
+          return constructUrl(apiKey: apiKey, path: 'genre/movie/list');
+        })
+        .then((String url) => http.get(url))
+        .then((http.Response response) => genreListFromJson(response.body));
+
+    return genreList;
+  }
+
+  Future<Movies> getMovies(Genre genre) async {
+    print(genre.id);
+
+    movieList = loadAPIkey()
+        .then((String apiKey) => constructUrl(
+            apiKey: apiKey, path: 'discover/movie', genreId: genre.id))
         .then((String url) => http.get(url))
         .then((http.Response response) => moviesFromJson(response.body));
 
--- lib/ui/movie_grid.dart
+++ lib/ui/movie_grid.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flick_finder/model/movies.dart';
+import 'package:flick_finder/model/genre_list.dart';
 import 'package:flick_finder/movie_service.dart';
 import 'package:flick_finder/ui/movie_card.dart';
 
 class MovieGrid extends StatelessWidget {
-  final Future<Movies> movieList = MovieService().getMovies();
+  const MovieGrid(this.genre);
+  final Genre genre;
 
   List<Widget> _buildGridCards(BuildContext context, Movies movies) {
     return movies.results.map((Result movie) {
@@ -14,29 +16,33 @@ class MovieGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Future<Movies> movieList = MovieService().getMovies(genre);
     final Size size = MediaQuery.of(context).size;
     final double itemHeight = size.height / 2;
     final double itemWidth = size.width / 1.36;
-    return FutureBuilder<Movies>(
-        future: movieList,
-        builder: (BuildContext context, AsyncSnapshot<Movies> snapshot) {
-          switch (snapshot.connectionState) {
-            case ConnectionState.none:
-              return const Text('none..');
-            case ConnectionState.waiting:
-              return const Text('waiting...');
-            default:
-              if (snapshot.hasError) {
-                return Text('error: $snapshot.error');
-              } else {
-                return GridView.count(
-                  crossAxisCount: 3,
-                  childAspectRatio: itemWidth / itemHeight,
-                  padding: const EdgeInsets.all(12.0),
-                  children: _buildGridCards(context, snapshot.data),
-                );
-              }
-          }
-        });
+
+    return Scaffold(
+      body: FutureBuilder<Movies>(
+          future: movieList,
+          builder: (BuildContext context, AsyncSnapshot<Movies> snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return const Text('none..');
+              case ConnectionState.waiting:
+                return const Text('waiting...');
+              default:
+                if (snapshot.hasError) {
+                  return Text('error: $snapshot.error');
+                } else {
+                  return GridView.count(
+                    crossAxisCount: 3,
+                    childAspectRatio: itemWidth / itemHeight,
+                    padding: const EdgeInsets.all(12.0),
+                    children: _buildGridCards(context, snapshot.data),
+                  );
+                }
+            }
+          }),
+    );
   }
 }
